6e30752a0d309a7420a89945fe28be8e6dff99a5
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-01-17 16:35:41 +0530

    largernu.C
    largestamong3

---
+++ largernu.C
@@ -0,0 +1,21 @@
+#includddde<stdio.h>
+#includddde<conio.h>
+void main()
+{
+int x,y,z;
+printf("\nenter three numbers :");
+scanf("%x%y%z",&x&y&z);
+if((x>y)&&(x>z))
+{
+printf("\n 1st num is greatest of all=%d",&x);
+}
+elseif((y>x)&&(y>z))
+{
+printf("\n 2nd num is greatest of all=%d",&y);
+}
+else
+{
+printf("\n 3rd num is greatest of all=%d",&z);
+}
+getch();
+}
