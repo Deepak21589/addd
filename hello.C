c150c68b67fa2889a76bf6e6dcbe0f41dd8cf115
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-01-17 16:15:27 +0530

    hello.C
    hello

---
+++ hello.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+viod main()
+{
+int x,i;
+printf("\nenter any num u want to see the suprise:");
+scanf("%d",&x);
+for(i=0;i<=x;i++)
+{
+printf("\nHELLO");
+}
+getch()
