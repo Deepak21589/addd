d4d02f21b3a2dca751d726e7f0d26a12aacaf286
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-01-21 16:51:29 +0530

    numint.C

---
+++ numint.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    long n;
+    int count = 0;
+
+    printf("\nEnter an integer: ");
+    scanf("%ld", &n);
+
+    while(n != 0)
+    {
+        n=n/10;
+        count++;
+    }
+
+    printf("\nNumber of digits: %d", count);
+    return 0;
+}
