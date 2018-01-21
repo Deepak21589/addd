40f70faecdb6b6e45bc3079839378f12db465864
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-01-21 16:32:31 +0530

    alphas1.C

---
+++ alphas1.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) 
+{
+char d;
+clrscr();
+printf("\n enter an num r alphabet :");
+scanf("%c",&d);
+if((d>='A' && d<='Z')||(d>='a' && d<='z'))
+{
+	printf("\nIt is an alphabet:%c",d);
+}
+else
+{
+	printf("it is not an alphabet%c",d);
+}
+	return 0;
+}
